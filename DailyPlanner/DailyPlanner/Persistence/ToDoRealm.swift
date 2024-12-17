f2b49e92a67398e341c8f77dfaf026a8e097afbb
Arsenty Halimovsky <ghost12@yandex.ru>
2024-12-17 06:54:02 +0300

    Create RealmToDo Object

--- DailyPlanner/DailyPlanner/Persistence/ToDoRealm.swift
+++ DailyPlanner/DailyPlanner/Persistence/ToDoRealm.swift
@@ -1,6 +1,35 @@
 import Foundation
 import RealmSwift
 
-struct ToDoRealm {
-	
+/// Object to store Realm-related `ToDo`
+final class ToDoRealm: Object {
+
+	// MARK: - Public Properties
+
+	@Persisted var id: UUID
+	@Persisted var title: String
+	@Persisted var discription: String
+	@Persisted var startDate: Date
+	@Persisted var endDate: Date
+	@Persisted var isCompleted: Bool
+
+	// MARK: - Initialization
+
+	convenience init(
+		id: UUID,
+		title: String,
+		discription: String,
+		startDate: Date,
+		endDate: Date,
+		isCompleted: Bool
+	) {
+
+		self.init()
+		self.id = id
+		self.title = title
+		self.discription = discription
+		self.startDate = startDate
+		self.endDate = endDate
+		self.isCompleted = isCompleted
+	}
 }
