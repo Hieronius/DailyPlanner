4cd107632a16b3bb2bb7260d44d5a95a7f3a028f
Arsenty Halimovsky <ghost12@yandex.ru>
2024-12-18 07:38:28 +0300

    add documentation

--- DailyPlanner/DailyPlanner/Screens/Main/Model/ToDoModel/TaskScreenMode.swift
+++ DailyPlanner/DailyPlanner/Screens/Main/Model/ToDoModel/TaskScreenMode.swift
@@ -1,7 +1,11 @@
 import Foundation
 
+/// Data type to define what mode we enter to
 enum TaskScreenMode {
 
+	/// `new task` button been pressed
 	case newTask
+
+	/// Specific task been selected
 	case detail(id: UUID)
 }
