079416c547cc287ced6b47a2d01d91a3954ce792
Arsenty Halimovsky <ghost12@yandex.ru>
2024-12-18 07:38:39 +0300

    add ToDoRealm conversion init()

--- DailyPlanner/DailyPlanner/Screens/Main/Model/ToDoModel/ToDo.swift
+++ DailyPlanner/DailyPlanner/Screens/Main/Model/ToDoModel/ToDo.swift
@@ -1,10 +1,25 @@
 import Foundation
 
+/// Model to hold a `ToDo` properties 
 struct ToDo: Hashable {
 	var id: UUID
-	var title: String = "Title"
-	var description: String = "Description"
+	var title: String = ""
+	var description: String = ""
 	var startDate: Date?
 	var endDate: Date?
 	var isCompleted: Bool = false
+}
+
+// MARK: - Convertion from `ToDoRealm`
+
+extension ToDo {
+	
+	init(object: ToDoRealm) {
+		id = object.id
+		title = object.title
+		description = object.discription
+		startDate = object.startDate
+		endDate = object.endDate
+		isCompleted = object.isCompleted
+	}
 }
